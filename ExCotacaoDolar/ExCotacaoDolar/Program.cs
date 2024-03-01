52a9bd691b3a3cc9bbf466e483dc98a510e8e4da
Matheus Rissi <58440654+matheus522@users.noreply.github.com>
2024-03-01 11:44:10 -0300

    Add files via upload
    Commit PC tabalho

---
+++ ExCotacaoDolar/ExCotacaoDolar/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExCotacaoDolar;
+
+Console.WriteLine("Qual a cotacao do dolar?");
+double cotacao = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantos dolares você vai comprar?");
+double dolar = double.Parse(Console.ReadLine());
+
+double reais = ConversorDeMoeda.calcular(cotacao, dolar);
+
+Console.WriteLine($"Valor a ser pago em Reais: R${reais:F2}");
---
+++ ExDadosDeFuncionario/ExDadosDeFuncionario/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExDadosDeFuncionario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double aumento)
+        {
+            SalarioBruto += SalarioBruto * aumento / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, Salario liquido{SalarioLiquido()}";
+        }
+
+
+    }
+}
