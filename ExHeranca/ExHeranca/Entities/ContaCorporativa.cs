ada5ee604ac275bbf9d266b4ed7d0f3022db6b3d
Rissi <mrissi2901@gmail.com>
2024-03-09 10:32:16 -0300

    New Commit

---
+++ ExHeranca/ExHeranca/Entities/ContaCorporativa.cs
@@ -0,0 +1,25 @@
+﻿namespace ExHeranca.Entities
+{
+    class ContaCorporativa : Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+
+        public ContaCorporativa() { }
+
+        public ContaCorporativa(int numero, string titular, double saldo, double limiteEmprestimo) : base(numero, titular, saldo)
+        {
+            LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public void emprestimo(double emprestimo)
+        {
+            if (emprestimo < LimiteEmprestimo)
+            {
+                Saldo += emprestimo;
+            }        
+        }
+
+
+    }
+}
