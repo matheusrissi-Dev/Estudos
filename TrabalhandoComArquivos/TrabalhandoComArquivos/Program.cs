f58b684f45cd95ee29182021b07c26127382e14a
Matheus Rissi <mrissi2901@gmail.com>
2024-05-15 13:08:39 -0300

    New Commit
    Inicio dos estudos com arquivos

---
+++ TrabalhandoComArquivos/TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TrabalhandoComArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\VisualStudio\arquivosParaTestes\file1.txt";
+            string targetPath = @"C:\VisualStudio\arquivosParaTestes\file2.txt";
+
+            try
+            {
+                FileInfo file = new FileInfo(sourcePath);
+                file.CopyTo(targetPath);
+
+                string[] linhas = File.ReadAllLines(sourcePath);
+                foreach (string item in linhas)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Ocorreu um erro: {e.Message}");
+            }
+        }
+    }
+}
