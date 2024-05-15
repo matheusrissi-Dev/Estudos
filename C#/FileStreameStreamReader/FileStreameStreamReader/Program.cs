e29c3d3e84e0fe697624071a735fb0e128a85fa8
Matheus Rissi <mrissi2901@gmail.com>
2024-05-15 13:39:33 -0300

    New commit
    FileStream e StreamReader

---
+++ C#/FileStreameStreamReader/FileStreameStreamReader/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FileStreameStreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\VisualStudio\Estudos\C#\arquivosParaTestes\file1.txt";
+            //FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                //fs = new FileStream(path, FileMode.Open);
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Ocorreu um erro: {e.Message}");
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                //if ( fs != null ) fs.Close();
+            }
+        }
+    }
+}
