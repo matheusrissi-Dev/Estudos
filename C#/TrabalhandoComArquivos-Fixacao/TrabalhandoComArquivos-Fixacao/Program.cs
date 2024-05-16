c1b7d40d7c113a42107493e291b8998c8d0fade5
Matheus Rissi <mrissi2901@gmail.com>
2024-05-16 15:58:26 -0300

    New commit
    Ex fixação

---
+++ C#/TrabalhandoComArquivos-Fixacao/TrabalhandoComArquivos-Fixacao/Program.cs
@@ -0,0 +1,47 @@
+﻿using TrabalhandoComArquivos_Fixacao.Entities;
+
+namespace TrabalhandoComArquivos_Fixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\VisualStudio\Estudos\C#\arquivosParaTestes\lista.csv";
+
+
+            try
+            {
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (var item in lines)
+                    {
+                        string[] campos = item.Split(',');
+                        string name = campos[0];
+                        double price = double.Parse(campos[1]);
+                        int quantidade = int.Parse(campos[2]);
+
+                        Product prod = new Product(name, price, quantidade);
+
+                        sw.WriteLine($"Nome: {prod.Name}, Preço: {prod.Total()}");
+                    }
+                }
+
+
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+    }
+}
