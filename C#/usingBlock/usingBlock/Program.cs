afd2aa9c002bd7ad756a598bdda20ca71578b339
Matheus Rissi <mrissi2901@gmail.com>
2024-05-16 09:58:41 -0300

    New Commit
    Aprendendo usar using Block

---
+++ C#/usingBlock/usingBlock/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace usingBlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\VisualStudio\Estudos\C#\arquivosParaTestes\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Ocorreu um erro: {e.Message}");
+            }
+        }
+    }
+}
