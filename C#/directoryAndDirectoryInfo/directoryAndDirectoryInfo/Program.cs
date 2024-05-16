5cd11917851f49d681854a01d68059e0d08804b9
Matheus Rissi <mrissi2901@gmail.com>
2024-05-16 10:44:28 -0300

    New Commit
    Directory

---
+++ C#/directoryAndDirectoryInfo/directoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace directoryAndDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\VisualStudio\Estudos\C#\arquivosParaTestes";
+
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Pastas: ");
+                foreach (var item in folders)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Arquivos: ");
+                foreach (var item in files)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Directory.CreateDirectory(path + @"\Nova_Pasta");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ocorreu um erro: {ex.Message}");
+            }
+
+        }
+    }
+}
---
+++ C#/streamWriter/streamWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace streamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\VisualStudio\Estudos\C#\arquivosParaTestes\file1.txt";
+            string targetPath = @"C:\VisualStudio\Estudos\C#\arquivosParaTestes\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string item in lines)
+                    {
+                        sw.WriteLine(item.ToUpper());
+                    }
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Um erro foi encontrado: {e.Message}");
+            }
+        }
+    }
+}
